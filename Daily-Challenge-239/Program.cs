3451ad2352bf72850856832d477e3a39f73075ac
encid <ryancav@gmail.com>
2017-01-04 09:06:26 -0500

    initial

--- Daily-Challenge-239/Program.cs
+++ Daily-Challenge-239/Program.cs
@@ -9,6 +9,61 @@ namespace DailyChallenge239
     {
         static void Main()
         {
+            int num;
+            string input;
+
+            Console.WriteLine("Game of Threes");            
+
+            do {
+                Console.Write("Input a number: ");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out num));
+
+            Calc(num);
+                                        
+            Console.ReadKey();
+        }
+
+        static void Calc(int num)
+        {
+            //do {
+	           // if (num % 3 == 0) {
+            //        Console.WriteLine("{0} 0", num);
+            //        num = num / 3;
+	           // }
+	           // else {
+	           //     if ((num + 1) % 3 == 0) {
+            //            Console.WriteLine("{0} 1", num);
+            //            num = (num + 1) / 3;	                    
+	           //     }
+	           //     else {
+            //            Console.WriteLine("{0} -1", num);
+            //            num = (num - 1) / 3;	                    
+	           //     }
+	           // }
+            //} while (num != 1);
+            //Console.WriteLine("1");
+
+
+            while (num != 1 && num != 0) {
+                if (num % 3 == 0) {
+                    Console.WriteLine("{0} 0", num);
+                    num = num / 3;
+                }
+                else {
+                    if ((num + 1) % 3 == 0) {
+                        Console.WriteLine("{0} 1", num);
+                        num = (num + 1) / 3;
+                    }
+                    else {
+                        Console.WriteLine("{0} -1", num);
+                        num = (num - 1) / 3;
+                    }
+                }
+            }
+            Console.WriteLine("1");
+
+            //return num;
         }
     }
 }
